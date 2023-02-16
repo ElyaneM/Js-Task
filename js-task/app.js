6175c3c924061203f51350eef70a17e23b4b7a1e
ElyaneMehiyeva <elyamehiyeva@gmail.com>
2023-02-16 19:25:46 +0400

    JS task

---
+++ js-task/app.js
@@ -0,0 +1,13 @@
+// let root = document.getElementById("root");
+// let input = document.querySelector("input[name=my_text]");
+// input.addEventListener("keyup", function (e) {
+//     e.preventDefault()
+//     let number = 0
+//     for (const herf of e.target.value) {
+//         number += herf.charCodeAt(0)
+//     }
+//     if (number > 100){
+//         number %= 100
+//     }
+//     root.innerText = number
+// })
